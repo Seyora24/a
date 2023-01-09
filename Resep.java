a7f5e422a102036ce7f3422cd24c577faabd262e
Seyora24 <114554755+Seyora24@users.noreply.github.com>
2023-01-09 18:19:23 +0700

    Add files via upload

---
+++ Resep.java
@@ -0,0 +1,13 @@
+
+public class Resep{
+
+public static void main(String[] args){
+System.out.println("Resep Nasi Padang");
+System.out.println("-----------------");
+System.out.println("1. Nasi");
+System.out.println("2. Bumbu");
+System.out.println("3. Sayur");
+System.out.println("4. Daging");
+
+}
+}
